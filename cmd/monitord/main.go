26db131bc2c97bddf1bf3dcad6c902825174029b
Isaac Blanco <nuclearhands2001@gmail.com>
2025-01-24 20:13:41 -0500

    refactor: Making the main.go per command

--- cmd/monitord/main.go
+++ cmd/monitord/main.go
@@ -1,34 +1,15 @@
-package monitord
+package main
 
 import (
 	terminal "alejandroblanco2001/scanneros/internal/terminal"
-	"context"
-	"runtime"
 
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
-func NewMonitor(lc fx.Lifecycle, log *zap.Logger) *terminal.Terminal {
-	os := runtime.GOOS
-
-	t := terminal.NewTerminal(os)
-
-	if t == nil {
-		panic("Error creating terminal")
-	}
-
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			log.Info("Terminal started")
-			t.Start()
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			t.Stop()
-			return nil
-		},
-	})
-
-	return t
+func main() {
+	fx.New(
+		fx.Options(
+			terminal.Module,
+		),
+	).Run()
 }
