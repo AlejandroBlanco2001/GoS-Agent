f9743f569f73a883aa002a3ada7256d910ec0a9f
Isaac Blanco <nuclearhands2001@gmail.com>
2025-01-26 17:14:11 -0500

    some more docs, new packaged and adding zeromq wrapper

--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -1,16 +1,12 @@
-// Purpose: Main entry point for the monitord application.
 package main
 
 import (
-	terminal "alejandroblanco2001/scanneros/internal/terminal"
-
 	"go.uber.org/fx"
 )
 
 func main() {
 	fx.New(
 		fx.Options(
-			terminal.Module,
 			// Remove the default logger from the fx module (it's not needed)
 			fx.NopLogger,
 		),
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/pebbe/zmq4 v1.2.11 // indirect
 	go.uber.org/dig v1.18.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
